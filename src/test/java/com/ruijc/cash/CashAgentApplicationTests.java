fb8ae1ccad16cbc5ea01887cc2dd467b226c5d49
storezhang <storezhang@gmail.com>
2017-09-22 21:30:26 +0800

    1、修复Docker打包；

--- src/test/java/com/ruijc/cash/CashAgentApplicationTests.java
+++ src/test/java/com/ruijc/cash/CashAgentApplicationTests.java
@@ -1,7 +1,9 @@
 package com.ruijc.cash;
 
+import com.ruijc.cash.shenqi.task.ShenqiTask;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CashAgentApplicationTests {
 
-    //@Autowired
-    //private ShenqiTask task;
+    @Autowired
+    private ShenqiTask task;
 
     @Test
     public void testShenqiCash() {
